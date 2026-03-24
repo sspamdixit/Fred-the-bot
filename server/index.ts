09c29d76477eb93bf8e4e56bdb6d29f88c2a9ea6
sspamdixit <spamdixit@gmail.com>
2026-03-24 17:36:22 +0530

    Update server/index.ts from Replit

--- server/index.ts
+++ server/index.ts
@@ -100,7 +100,7 @@ app.use((req, res, next) => {
     await setupVite(httpServer, app);
   }
 
-  const port = parseInt(process.env.PORT || "3000", 10);
+  const port = parseInt(process.env.PORT || "5000", 10);
   httpServer.listen(
     {
       port,
